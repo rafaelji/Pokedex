4dea2194779187b399016b2ff7930bfe5e95d527
Rafael Barbosa Silva <r.ji@outlook.com>
2020-10-14 18:46:15 -0300

    add jquery, popper.js and import bootstrap js file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
