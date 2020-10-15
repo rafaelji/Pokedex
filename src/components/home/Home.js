f3120b0d2961c5c899783f082395c97dabe85b7d
Rafael Barbosa Silva <r.ji@outlook.com>
2020-10-15 16:14:15 -0300

    add home page content

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import Pokedex from './pokedex.png';
 import './Home.css';
 
+import {Link} from "react-router-dom";
+
 function Home(props) {
-  return <h1>Home</h1>;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 pt-5 text-center">
+          <h1>Pokédex Online</h1>
+          <img src={Pokedex} className="pokedex" alt="Pokédex"/>
+          <br />
+          <Link to="/app" style={{marginTop: '10rem'}} className="btn btn-primary w-50">Enter</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
