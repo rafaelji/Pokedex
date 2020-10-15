f0f08e0d46bae7ac506db286a966a82ccb0e830b
Rafael Barbosa Silva <r.ji@outlook.com>
2020-10-15 16:25:19 -0300

    add about content

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,7 +3,18 @@ import React from 'react';
 import './About.css';
 
 function About() {
-  return <h1>About</h1>;
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 pt-5 text-center">
+            <h1 className="mt-5">About</h1>
+            <p className="mt-5">Pokédex Online created by <a href="https://www.linkedin.com/in/rafaelbarbosasilva/">Rafael Barbosa Silva</a></p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
