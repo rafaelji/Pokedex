0bcf87b850a4dcd936eff56fb4fd22d7243a9ea6
Rafael Barbosa Silva <r.ji@outlook.com>
2020-10-15 00:12:05 -0300

    add shell navbar

--- src/components/shell/Shell.js
+++ src/components/shell/Shell.js
@@ -3,7 +3,31 @@ import React from 'react';
 import './Shell.css';
 
 function Shell() {
-  return <h1>Shell</h1>;
+  return (
+    <div className="shell">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Pokédex</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Pokédex</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Shell;
