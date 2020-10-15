e4ffb59019f1f3ed2dd082e839850e3b951be384
Rafael Barbosa Silva <r.ji@outlook.com>
2020-10-15 00:05:15 -0300

    add main routes to app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from "./components/home/Home";
+import Shell from "./components/shell/Shell";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/app" component={Shell} />
+      </Switch>
+    </Router>
   );
 }
 
