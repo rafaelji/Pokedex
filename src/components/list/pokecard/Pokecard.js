0ff309374cd532756c59bc0796f73c6a1bc07f7e
Rafael Barbosa Silva <r.ji@outlook.com>
2020-10-15 14:26:58 -0300

    css class refactor

--- src/components/list/pokecard/Pokecard.js
+++ src/components/list/pokecard/Pokecard.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Link, useRouteMatch} from "react-router-dom";
 
+import './Pokecard.css';
+
 function PokeCard(props) {
   const match = useRouteMatch();
 
   return (
-    <div className="card">
+    <div className="card poke-card">
       <img src={`${process.env.PUBLIC_URL}/sprites/${props.index + 1}.png`} className="card-img-top" alt={props.name} />
       <div className="card-body text-center">
-        <Link to={`${match.url}/${props.name}`} className="btn btn-primary w-100">{props.name}</Link>
+        <Link to={`${match.url}/${props.index + 1}`} className="btn btn-primary w-100">{props.name}</Link>
       </div>
     </div>
   );
